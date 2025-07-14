926024348fc2ffd780408bda53d5594188d3f580
Ghieta Maureen <ghieta.maureen@gmail.com>
2025-07-15 00:56:39 +0700

    fix home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 <div class="home-container" style="display: flex; justify-content: center; align-items: center; padding: 100px 80px; gap: 80px; flex-wrap: wrap;">
   <!-- Kolom Kiri -->
   <div class="home-content" style="flex: 1; max-width: 750px; text-align: center;">
-    <h1 style="font-size: 60px; font-weight: bold; line-height: 1.5; margin-bottom: 40px;">
+    <h1 style="font-size: 45px; font-weight: bold; line-height: 1.5; margin-bottom: 40px;">
       <span style="color: #167DD3;">Modifikasi</span> motor anda dengan<br>
       Aman dan Terpercaya di Bengkel<br>
       <span style="color: #167DD3;">Locomotif Jajar.</span>
@@ -15,7 +15,7 @@
 
     <a
       href="https://wa.me/628176820332"
-      style="display: inline-block; width: 400px; height: 90px; background-color: #E2EAF4; border-radius: 12px; line-height: 90px; font-size: 26px; font-weight: 600; color: black; text-decoration: none; margin-bottom: 50px;"
+      style="display: inline-block; width: 400px; height: 90px; background-color: #E2EAF4; border-radius: 12px; line-height: 90px; font-size: 24px; font-weight: 600; color: black; text-decoration: none; margin-bottom: 50px;"
       onmouseover="this.style.opacity='0.9'"
       onmouseout="this.style.opacity='1'"
     >
@@ -41,7 +41,7 @@
     <img
       src="{{ asset('img/coverhome.jpeg') }}"
       alt="Cover Home"
-      style="width: 100%; max-width: 600px; height: auto; border-radius: 12px; object-fit: cover;"
+      style="width: 425px; max-width: 648px; height: auto; border-radius: 12px; object-fit: cover;"
     />
   </div>
 </div>
