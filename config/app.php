4a145e474037bc99085f6035cbefd7d0a5a92b1f
Ghieta Maureen <ghieta.maureen@gmail.com>
2025-07-16 08:56:37 +0700

    upload tf

--- config/app.php
+++ config/app.php
@@ -123,17 +123,4 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    'providers' => [
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
-    ],
-
-    'aliases' => [
-        'Storage' => Illuminate\Support\Facades\Storage::class,
-        'Cloudinary' => CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::class,
-    ],
-
-
-    
-
 ];
